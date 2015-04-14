e9a1f6b08cebd3acf39e5270c6c01e029de78f39
sorellan <s.salcedo.orellana@gmail.com>
2015-04-14 14:38:07 +0200

    Acabado 5.9.5

--- JQuery/jquery-ui-1.11.4/ejercicios/pruebajqueryui.js
+++ JQuery/jquery-ui-1.11.4/ejercicios/pruebajqueryui.js
@@ -1,3 +1,25 @@
 jQuery(document).ready(function() {
 	$("#date").datepicker();
+	$("#menu").menu();
+	$("#menu").menu({
+  		icons: { submenu: "ui-icon-squaresmall-plus" }
+	});
+	$("#draggable").draggable();
+	$("#draggable2").draggable({ revert: "valid" });
+	$("#droppable").droppable({
+      drop: function( event, ui ) {
+        $(this)
+          .addClass("ui-state-highlight")
+          .find("p")
+            .html("Dropped!");
+      }
+    });
+    $("#droppable").droppable({
+      out: function( event, ui ) {
+      	$(this)
+      	  .removeClass("ui-state-highlight")
+      	  .find("p")
+      	    .html("Drop here");
+      }
+    });
 });
--- Leaflet/nominatim/nominatim.js
+++ Leaflet/nominatim/nominatim.js
@@ -1,6 +1,8 @@
+var map;
+
 jQuery(document).ready(function() {
 	// create a map in the "map" div, set the view to a given place and zoom
-	var map = L.map('map');
+	map = L.map('map');
 
 	// add an OpenStreetMap tile layer
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -32,4 +34,44 @@ jQuery(document).ready(function() {
     	alert(e.message);
 	}
 	map.on('locationerror', onLocationError);
+
+
 });
+
+function chooseAddr(lat, lng, type) {
+  	var location = new L.LatLng(lat, lng);
+  	map.panTo(location);
+
+  	if (type == 'city' || type == 'administrative') {
+    	map.setZoom(11);
+  	} else {
+    	map.setZoom(13);
+  	}
+}
+
+function addr_search() {
+  	var inp = document.getElementById("addr");
+
+  	$.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=5&q=' + inp.value, function(data) {
+  		var items = [];
+
+		$.each(data, function(key, val) {
+  			items.push(
+    		"<li><a href='#' onclick='chooseAddr(" +
+    		val.lat + ", " + val.lon + ");return false;'>" + val.display_name +
+    		'</a></li>'
+    		);
+  		});
+
+  		$('#results').empty();
+    	if (items.length != 0) {
+      		$('<p>', { html: "Search results:" }).appendTo('#results');
+      		$('<ul/>', {
+        		'class': 'my-new-list',
+        		html: items.join('')
+      		}).appendTo('#results');
+    	} else {
+      		$('<p>', { html: "No results found" }).appendTo('#results');
+    	}  		
+  	});	
+}
